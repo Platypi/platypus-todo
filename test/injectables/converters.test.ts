ccc029dca6d46c485a4b481a7fa6b577c0ddaa4a
Will Johnston <will@getplatypi.com>
2015-05-13 09:50:25 -0500

    tests

--- test/injectables/converters.test.ts
+++ test/injectables/converters.test.ts
@@ -2,10 +2,18 @@
 
 describe('Testing converters to', () => {
 	describe('make sure pluralize', () => {
+		var converters: app.Converters;
+		
+		beforeEach(() => {
+			converters = plat.acquire(app.Converters);
+		});
+		
 		it('pluralizes values when length !== 1', () => {
-			var converters: app.Converters = plat.acquire(app.Converters);
-			
 			expect(converters.pluralize('hat', 0)).toBe('hats');
+		});
+
+		it('does not pluralize values when length === 1', () => {
+			expect(converters.pluralize('hat', 1)).toBe('hat');
 		});
 	});
 });
